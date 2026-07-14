d63bba0303c4c13595143e391562dacbd5683e83
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-14 19:47:47 +0530

    Added db connection.

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: ".env.local" });
+const processENV = process.env;
+const db = new pg.Client({
+    user: processENV.DB_USER,
+    host: processENV.DB_HOST,
+    password: processENV.DB_PASS,
+    port: processENV.DB_PORT,
+    database: processENV.DB_NAME
+})
+
+export default db;
