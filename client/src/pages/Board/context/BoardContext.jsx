788d33e020137ca070684e13b3183fe56fcd711e
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-22 16:39:35 +0530

    Implemented board context api

---
+++ client/src/pages/Board/context/BoardContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useState } from "react";
+import { TOOLS } from "../../../utils/constants";
+
+export const BoardContext = createContext(null);
+
+const BoardProvider = ({ children }) => {
+	const [selectedTool, setSelectedTool] = useState(TOOLS.SELECT);
+	const [isDrawing, setIsDrawing] = useState(false);
+	const [currentElement, setCurrentElement] = useState(null);
+	const [elements, setElements] = useState([
+		{
+			id: 1,
+			element_type: "rectangle",
+			element_data: {
+				x: 100,
+				y: 100,
+				width: 200,
+				height: 100,
+				fill: "#fff",
+				stroke: "#000",
+				strokeWidth: 2
+			}
+		}
+	]
+	);
+	const [selectedElementId, setSelectedElementId] = useState(null);
+	const value = {
+		selectedTool,
+		setSelectedTool,
+
+		isDrawing,
+		setIsDrawing,
+
+		currentElement,
+		setCurrentElement,
+
+		elements,
+		setElements,
+
+		selectedElementId,
+		setSelectedElementId
+	};
+
+	return (
+		<BoardContext.Provider value={value}>
+			{children}
+		</BoardContext.Provider>
+	)
+}
+export default BoardProvider;
