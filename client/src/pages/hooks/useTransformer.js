9ae1ec100f1aa42c6a670d293e48f5c390d90cb7
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-23 16:54:18 +0530

    Implemented useTransfromer custom hook.

---
+++ client/src/pages/hooks/useTransformer.js
@@ -0,0 +1,19 @@
+import React, { useContext, useEffect } from 'react'
+import { BoardContext } from '../Board/context/BoardContext';
+
+const useTransformer = ({ transformerRef, rectangleRefs }) => {
+	const { selectedElementId } = useContext(BoardContext);
+	useEffect(() => {
+		if (!selectedElementId) {
+			transformerRef.current.nodes([]);
+			transformerRef.current.getLayer().batchDraw();
+			return;
+		}
+		const selectedNode = rectangleRefs.current[selectedElementId];
+		if (!selectedNode) return;
+		transformerRef.current.nodes([selectedNode]);
+		transformerRef.current.getLayer().batchDraw();
+	}, [selectedElementId])
+}
+
+export default useTransformer
