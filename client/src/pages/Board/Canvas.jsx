36236a0a8c4d61626a71e27e163c55c697a835d2
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-23 17:39:05 +0530

    Made use of custom hooks.

--- client/src/pages/Board/Canvas.jsx
+++ client/src/pages/Board/Canvas.jsx
@@ -1,93 +1,64 @@
 import { Stage, Layer } from 'react-konva';
-import React, { useContext, useEffect, useState } from 'react'
+import { useContext, useRef } from 'react'
 import Rectangle from './elements/Rectangle';
 import { BoardContext } from './context/BoardContext';
-import { TOOLS } from '../../utils/constants';
+import { Transformer } from 'react-konva';
+import useDrawing from '../hooks/useDrawing';
+import useTransformer from '../hooks/useTransformer';
+import useSelection from '../hooks/useSelection';
+import useKeyboard from '../hooks/useKeyboard';
 
 const Canvas = () => {
-  const { selectedTool,
-    isDrawing, setIsDrawing,
-    currentElement, setCurrentElement,
-    elements, setElements } = useContext(BoardContext);
+	const { currentElement,
+		elements,
+	} = useContext(BoardContext);
+	const transformerRef = useRef(null);
+	const rectangleRefs = useRef({});
+	const { handleMouseDown, handleMouseMove, handleMouseUp } = useDrawing();
+	const { handleStageDown } = useSelection();
 
-  // const { selectedElementId } = useContext(BoardContext);
+	useKeyboard({ rectangleRefs });
+	useTransformer({ transformerRef, rectangleRefs });
 
-  // useEffect(() => {
-  //   console.log("Canvas:", selectedElementId);
-  // }, [selectedElementId]);
-  const handleMouseDown = (e) => {
-    if (selectedTool !== TOOLS.RECTANGLE) return;
-    if (e.target !== e.target.getStage()) return;
-    const stage = e.target.getStage();
-    const position = stage.getPointerPosition();
-    setIsDrawing(true);
-    setCurrentElement({
-      id: Date.now(),
-      element_type: "rectangle",
-      element_data: {
-        x: position.x,
-        y: position.y,
-        width: 0,
-        height: 0,
-        fill: "#fff",
-        stroke: "#000",
-        strokeWidth: 2,
-      },
-    });
-  }
-
-  const handleMouseMove = (e) => {
-    if (selectedTool !== TOOLS.RECTANGLE) return;
-    if (!isDrawing) return;
-    const stage = e.target.getStage();
-    const position = stage.getPointerPosition();
-    setCurrentElement((prev) => ({
-      ...prev,
-      element_data: {
-        ...prev.element_data,
-        width: position.x - prev.element_data.x,
-        height: position.y - prev.element_data.y
-      }
-    }))
-
-  }
-  const handleMouseUp = (e) => {
-    if (!isDrawing) return;
-    setElements((prev) => [...prev, currentElement]);
-    setCurrentElement(null);
-    setIsDrawing(false);
-  }
-
-  return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        background: "#f5f5f5",
-      }}
-    >
-      <Stage
-        width={window.innerWidth}
-        height={window.innerHeight}
-        onMouseMove={handleMouseMove}
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}>
-        <Layer>
-          {
-            elements.map((element) => (
-              <Rectangle key={element.id}
-                element={element}
-              />
-            ))
-          }
-          {
-            currentElement &&
-            <Rectangle element={currentElement} />
-          }
-        </Layer>
-      </Stage>
-    </div>
-  )
+	return (
+		<div
+			style={{
+				width: "100vw",
+				height: "100vh",
+				background: "#f5f5f5",
+			}}
+		>
+			<Stage
+				width={window.innerWidth}
+				height={window.innerHeight}
+				onMouseMove={handleMouseMove}
+				onMouseDown={(e) => {
+					handleMouseDown(e)
+					handleStageDown(e)
+				}}
+				onMouseUp={handleMouseUp}>
+				<Layer>
+					<Transformer ref={transformerRef} />
+					{
+						elements.map((element) => (
+							<Rectangle key={element.id}
+								element={element}
+								ref={(node) => {
+									if (node) {
+										rectangleRefs.current[element.id] = node;
+									}
+								}}
+							/>
+						))
+					}
+					{
+						currentElement &&
+						<Rectangle element={currentElement} />
+					}
+				</Layer>
+			</Stage>
+		</div>
+	)
 }
 
 export default Canvas
