2d20f0e9defc66e40378207ccf29519799885698
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-26 19:53:33 +0530

    Refactored code.

--- client/src/pages/hooks/useDrawing.js
+++ client/src/pages/hooks/useDrawing.js
@@ -1,6 +1,8 @@
 import { useContext } from 'react'
 import { BoardContext } from '../Board/context/BoardContext';
 import { TOOLS } from '../../utils/constants';
+import elementRegistry from '../Board/elements/elementRegistry';
+import drawingRegistry from '../Board/elements/drawingRegistry';
 
 const useDrawing = () => {
   const { selectedTool,
@@ -10,45 +12,87 @@ const useDrawing = () => {
   } = useContext(BoardContext);
 
   const handleMouseDown = (e) => {
-    if (selectedTool !== TOOLS.RECTANGLE) return;
+    if (!selectedTool) return;
     if (e.target !== e.target.getStage()) return;
     const stage = e.target.getStage();
     const position = stage.getPointerPosition();
     setIsDrawing(true);
-    setCurrentElement({
-      id: Date.now(),
-      element_type: "rectangle",
-      element_data: {
-        x: position.x,
-        y: position.y,
-        width: 0,
-        height: 0,
-        fill: "#fff",
-        stroke: "#000",
-        strokeWidth: 2,
-      },
-    });
+    if (selectedTool === TOOLS.RECTANGLE) {
+      setCurrentElement(drawingRegistry.rectangle(position));
+    } else if (selectedTool === TOOLS.LINE || selectedTool === TOOLS.ARROW) {
+      setCurrentElement(drawingRegistry.line(position, selectedTool));
+    } else if (selectedTool === TOOLS.CIRCLE) {
+      setCurrentElement(drawingRegistry.circle(position));
+    }
   }
 
   const handleMouseMove = (e) => {
-    if (selectedTool !== TOOLS.RECTANGLE) return;
+    if (!selectedTool) return;
     if (!isDrawing) return;
     const stage = e.target.getStage();
     const position = stage.getPointerPosition();
-    setCurrentElement((prev) => ({
-      ...prev,
-      element_data: {
-        ...prev.element_data,
-        width: position.x - prev.element_data.x,
-        height: position.y - prev.element_data.y
-      }
-    }))
+    if (selectedTool === TOOLS.RECTANGLE) {
+      setCurrentElement((prev) => {
+        if (!prev) return null;
+
+        return {
+          ...prev,
+          element_data: {
+            ...prev.element_data,
+            width: position.x - prev.element_data.x,
+            height: position.y - prev.element_data.y,
+          },
+        };
+      });
+    } else if (selectedTool === TOOLS.LINE || selectedTool === TOOLS.ARROW) {
+      setCurrentElement((prev) => {
+        if (!prev) return null;
+
+        return {
+          ...prev,
+          element_data: {
+            ...prev.element_data,
+            points: [
+              prev.element_data.points[0],
+              prev.element_data.points[1],
+              position.x,
+              position.y,
+            ],
+          },
+        };
+      });
+    } else if (selectedTool === TOOLS.CIRCLE) {
+      const position = stage.getPointerPosition();
+      setCurrentElement((prev) => {
+        if (!prev) return null;
+        const dx = position.x - prev.element_data.x;
+        const dy = position.y - prev.element_data.y;
+        const radius = Math.hypot(dx, dy);
+        return {
+          ...prev,
+          element_data: {
+            ...prev.element_data,
+            radius
+          },
+        };
+      });
+    }
 
   }
 
   const handleMouseUp = () => {
     if (!isDrawing) return;
-    setElements((prev) => [...prev, currentElement]);
+    if (currentElement) {
+      const registryItem = elementRegistry[currentElement.element_type];
+
+      if (!registryItem.isValid(currentElement)) {
+        setCurrentElement(null);
+        setIsDrawing(false);
+        return;
+      }
+
+      setElements((prev) => [...prev, currentElement]);
+    }
     setCurrentElement(null);
     setIsDrawing(false);
   }
