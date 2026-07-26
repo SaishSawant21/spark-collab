a23bc0a2f573f5860b6db5c48c86a56650fa7d92
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-26 20:02:34 +0530

    Implemented  element registry  for validation of elements.

---
+++ client/src/pages/Board/elements/elementRegistry.js
@@ -0,0 +1,39 @@
+import ArrowElement from "./ArrowElement";
+import CircleElement from "./CircleElement";
+import LineElement from "./LineElement";
+import Rectangle from "./Rectangle";
+
+const elementRegistry = {
+	rectangle: {
+		component: Rectangle,
+		isValid: (element) => {
+			return (
+				Math.abs(element.element_data.width) > 0 &&
+				Math.abs(element.element_data.height) > 0
+			);
+		},
+	},
+
+	line: {
+		component: LineElement,
+		isValid: (element) => {
+			const [x1, y1, x2, y2] = element.element_data.points;
+			return !(x1 === x2 && y1 === y2)
+		},
+	},
+
+	circle: {
+		component: CircleElement,
+		isValid: (element) => {
+			return element.element_data.radius > 0;
+		},
+	},
+	arrow: {
+		component: ArrowElement,
+		isValid: (element) => {
+			const [x1, y1, x2, y2] = element.element_data.points;
+			return !(x1 === x2 && y1 === y2)
+		},
+	},
+};
+export default elementRegistry;
