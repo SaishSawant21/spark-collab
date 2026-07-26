e4a66636f525b98eb92b45a35799d9bb5c732bec
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-26 19:50:59 +0530

    Implement registry to store drawing functions.

---
+++ client/src/pages/Board/elements/drawingRegistry.js
@@ -0,0 +1,44 @@
+const DEFAULT_STYLES = {
+  fill: "#fff",
+  stroke: "#000",
+  strokeWidth: 2,
+};
+
+const drawingRegistry = {
+  rectangle: (position) => ({
+    id: Date.now(),
+    element_type: "rectangle",
+    element_data: {
+      x: position.x,
+      y: position.y,
+      width: 0,
+      height: 0,
+      ...DEFAULT_STYLES
+    },
+  }),
+  circle: (position) => ({
+    id: Date.now(),
+    element_type: "circle",
+    element_data: {
+      x: position.x,
+      y: position.y,
+      radius: 0,
+      ...DEFAULT_STYLES
+    },
+  }),
+  line: (position, elementType = "line") => ({
+    id: Date.now(),
+    element_type: elementType,
+    element_data: {
+      points: [
+        position.x,
+        position.y,
+        position.x,
+        position.y,
+      ],
+      stroke: DEFAULT_STYLES.stroke,
+      strokeWidth: DEFAULT_STYLES.strokeWidth,
+    },
+  }),
+};
+export default drawingRegistry;
