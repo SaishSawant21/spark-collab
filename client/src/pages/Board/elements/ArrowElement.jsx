8379a69bab57ea7a86584f8cc275d5e51f10cf1f
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-31 18:04:52 +0530

    Added tranform logic for arrow element.

--- client/src/pages/Board/elements/ArrowElement.jsx
+++ client/src/pages/Board/elements/ArrowElement.jsx
@@ -1,14 +1,21 @@
 import { Arrow } from "react-konva";
 import useElementInteractions from "../../hooks/useElementInteractions";
+import { forwardRef } from "react";
 
-const ArrowElement = ({ element }) => {
+const ArrowElement = forwardRef(({ element }, ref) => {
   const interactions = useElementInteractions(element);
   return (
     <Arrow
+      ref={ref}
       points={element.element_data.points}
       stroke={element.element_data.stroke}
       strokeWidth={element.element_data.strokeWidth}
+      hitStrokeWidth={20}
+      draggable={interactions.draggable}
+      onMouseDown={interactions.handleSelect}
+      onDragEnd={interactions.handleDragEnd}
+      onTransformEnd={interactions.transformElement}
     />
   )
-}
+})
 export default ArrowElement;
