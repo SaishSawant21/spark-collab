b435cadb939f979b0393eff7180bba4a35811252
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-31 17:55:08 +0530

    Added tranform logic for circle.

--- client/src/pages/Board/elements/CircleElement.jsx
+++ client/src/pages/Board/elements/CircleElement.jsx
@@ -1,10 +1,12 @@
-import { forwardRef } from "react";
+import { forwardRef, useContext } from "react";
 import { Circle } from "react-konva";
 import useElementInteractions from "../../hooks/useElementInteractions";
+import { BoardContext } from "../context/BoardContext";
 
 const CircleElement = forwardRef(({ element }, ref) => {
   const elementData = element.element_data;
   const interactions = useElementInteractions(element);
+  const { selectedElementId } = useContext(BoardContext);
   return (
     <Circle
       ref={ref}
@@ -12,9 +14,12 @@ const CircleElement = forwardRef(({ element }, ref) => {
       y={elementData.y}
       radius={elementData.radius}
       fill={elementData.fill}
-      stroke={elementData.stroke}
-      strokeWidth={elementData.strokeWidth}
+      stroke={selectedElementId === element.id ? '#1677ff' : element.element_data.stroke}
+      strokeWidth={selectedElementId === element.id ? 3 : element.element_data.strokeWidth}
       draggable={interactions.draggable}
+      onMouseDown={interactions.handleSelect}
+      onDragEnd={interactions.handleDragEnd}
+      onTransformEnd={interactions.transformElement}
     />
   )
 })
