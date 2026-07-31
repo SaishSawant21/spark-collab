54a08435e8e4ed79ea14f455ecacec3e7716a442
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-31 17:57:59 +0530

    Implemented shortcut keys for editor.

---
+++ client/src/pages/hooks/useKeyboardShortcuts.js
@@ -0,0 +1,41 @@
+import { useCallback, useContext, useEffect } from "react";
+import { BoardContext } from "../Board/context/BoardContext";
+
+const useKeyboardShortcuts = () => {
+  const { undo, redo,
+    selectedElementId, setSelectedElementId,
+    updateElements,
+    duplicateSelectedElement
+  } = useContext(BoardContext);
+  const handleKeyDown = useCallback((e) => {
+    if (e.ctrlKey && e.key.toLowerCase() === "z" && !e.shiftKey) {
+      e.preventDefault();
+      undo();
+    } else if (e.ctrlKey && e.key.toLowerCase() === "y") {
+      e.preventDefault();
+      redo();
+    } else if (e.key === "Delete" && selectedElementId) {
+      e.preventDefault();
+
+      updateElements((prev) =>
+        prev.filter((item) => item.id !== selectedElementId)
+      );
+      setSelectedElementId(null);
+    } else if (e.ctrlKey && e.key.toLowerCase() === "d") {
+      e.preventDefault();
+      duplicateSelectedElement();
+    } else if (e.key === "Escape") {
+      setSelectedElementId(null);
+    }
+  }, [undo, redo, updateElements, selectedElementId])
+  useEffect(() => {
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+
+  }, [handleKeyDown])
+}
+
+export default useKeyboardShortcuts
