fc7e7ab3fe626d84c82a9c1d1f251ef5c488b91d
SaishSawant21 <saishsawant25102001@gmail.com>
2026-07-31 17:55:39 +0530

    Added transform logic for line element.

--- client/src/pages/Board/elements/LineElement.jsx
+++ client/src/pages/Board/elements/LineElement.jsx
@@ -1,12 +1,21 @@
 import { Line } from "react-konva"
-const LineElement = ({ element }) => {
+import useElementInteractions from "../../hooks/useElementInteractions";
+import { forwardRef } from "react";
+const LineElement = forwardRef(({ element }, ref) => {
+  const interactions = useElementInteractions(element);
   return (
     <Line
+      ref={ref}
       points={element.element_data.points}
       stroke={element.element_data.stroke}
       strokeWidth={element.element_data.strokeWidth}
+      hitStrokeWidth={20}
+      draggable={interactions.draggable}
+      onMouseDown={interactions.handleSelect}
+      onDragEnd={interactions.handleDragEnd}
+      onTransformEnd={interactions.transformElement}
     />
   )
-}
+})
 
 export default LineElement;
