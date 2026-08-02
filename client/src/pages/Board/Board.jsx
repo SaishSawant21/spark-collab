caaa90e826c208eb3e1236286caf23337a5bc2cb
SaishSawant21 <saishsawant25102001@gmail.com>
2026-08-02 20:23:44 +0530

    Removed BoardProvider import.

--- client/src/pages/Board/Board.jsx
+++ client/src/pages/Board/Board.jsx
@@ -1,13 +1,10 @@
 import Canvas from './Canvas'
-import BoardProvider from './context/BoardProvider'
+import BoardProvider from '../../context/BoardProvider'
 import ToolBar from './toolbar/ToolBar'
 
 const Board = () => {
   return (
-    <BoardProvider>
-      <ToolBar />
-      <Canvas />
-    </BoardProvider>
+    <Canvas />
   )
 }
 
