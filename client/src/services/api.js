bd4fe911126bf874deed83003528eef4d2bee609
SaishSawant21 <saishsawant25102001@gmail.com>
2026-08-10 20:49:06 +0530

    Changed the return for all  functions.

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,30 +1,33 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
 export const fetchApi = async (url, params = {}) => {
-  console.log("Base url: ", API_BASE_URL);
-  return axios.get(`${API_BASE_URL}${url}`, {
+  const res = await axios.get(`${API_BASE_URL}${url}`, {
     params,
     withCredentials: true,
   });
+
+  return res?.data;
 };
 
 export const submitApi = async (url, payload = {}) => {
-  return axios.post(`${API_BASE_URL}${url}`, payload, {
+  const res = await axios.post(`${API_BASE_URL}${url}`, payload, {
     withCredentials: true,
   });
+  return res?.data;
 };
 
 export const updateApi = async (url, payload = {}) => {
-  return axios.put(`${API_BASE_URL}${url}`, payload, {
+  const res = await axios.put(`${API_BASE_URL}${url}`, payload, {
     withCredentials: true,
   });
+  return res?.data;
 };
 
 export const deleteApi = async (url, payload = {}) => {
-  return axios.delete(`${API_BASE_URL}${url}`, {
+  const res = await axios.delete(`${API_BASE_URL}${url}`, {
     data: payload,
     withCredentials: true,
   });
+  return res?.data;
 };
