fd3ccfef86532be954611533103c748a126951b2
SaishSawant21 <saishsawant25102001@gmail.com>
2026-08-10 20:56:47 +0530

    Implemented login.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,33 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { socket } from "./socket";
 import Board from "./pages/Board/Board";
 import AppLayout from "./layouts/AppLayout";
 import BoardProvider from "./context/BoardProvider";
+import { authenticateUser } from "./services/authService";
+import AuthProvider from "./context/AuthProvider";
+import { AuthContext } from "./context/AuthContext";
+import Login from "./pages/Auth/Login";
 
 function App() {
+  const AppContent = () => {
+    const { isAuthenticated } = useContext(AuthContext);
+    if (!isAuthenticated) {
+      return <Login />;
+    }
+
+    return (
+      <BoardProvider>
+        <AppLayout>
+          <Board />
+        </AppLayout>
+      </BoardProvider>
+    );
+  };
 
   return (
-    <BoardProvider>
-      <AppLayout>
-        <Board />
-      </AppLayout>
-    </BoardProvider>
+    <AuthProvider>
+      <AppContent />
+    </AuthProvider>
   )
 }
 
---
+++ client/src/pages/Auth/Login.jsx
@@ -0,0 +1,59 @@
+import { Button, Flex, Form, Input, message, Typography } from "antd"
+import { messageContants } from "../../utils/constants"
+import { useContext, useState } from "react"
+import { authenticateUser } from "../../services/authService";
+import { AuthContext } from "../../context/AuthContext";
+
+const Login = () => {
+	const [loading, setLoading] = useState(false);
+	const { setUser } = useContext(AuthContext);
+
+	const onSubmit = async (values) => {
+		try {
+			setLoading(true);
+			const res = await authenticateUser(values);
+			if (res.code === 200) {
+				message.success(res?.message);
+				setUser({
+					username: values?.username
+				})
+			}
+		} catch (error) {
+			message.error(error?.response?.data?.message || messageContants.somethingWerntWrong);
+		} finally {
+			setLoading(false);
+		}
+
+	}
+	const rules = {
+		required: true,
+		message: messageContants.requiredMsg
+	}
+	return (
+		<Flex justify="center" align="center" className="min-h-screen">
+			<div className="w-full max-w-sm">
+				<Typography.Title level={2}>
+					Login
+				</Typography.Title>
+				<Form layout='vertical' onFinish={onSubmit}>
+					<Form.Item name={'username'} label='Username'
+						rules={[
+							rules
+						]}>
+						<Input />
+					</Form.Item>
+					<Form.Item name={'password'} label='Password'
+						rules={[
+							rules
+						]}>
+						<Input type={'password'} />
+					</Form.Item>
+					<Button htmlType="submit" type="primary" block loading={loading}>
+						Login
+					</Button>
+				</Form>
+			</div>
+		</Flex>
+	)
+}
+export default Login;
