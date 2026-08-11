a4480fcd04894dd184978d191d0e9a53d4040107
SaishSawant21 <saishsawant25102001@gmail.com>
2026-08-11 20:24:30 +0530

    Added board dropdown.

--- client/src/layouts/components/Header.jsx
+++ client/src/layouts/components/Header.jsx
@@ -5,19 +5,24 @@ import {
 } from "@ant-design/icons";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { BoardsContext } from "../../context/BoardsContext";
+import { useNavigate } from "react-router-dom";
 
 const { Header } = Layout;
 const { Title } = Typography;
 
 const AppHeader = () => {
   const { logout } = useContext(AuthContext);
-  const menuItem = [
+  const { boards, loading } = useContext(BoardsContext);
+  const navigate = useNavigate();
+  const menuItems = [
     {
       key: "logout",
       label: "Logout",
       onClick: logout,
     },
   ]
+
   return (
     <Header className="!bg-white border-b border-slate-200 px-6">
       <Flex
@@ -33,6 +38,21 @@ const AppHeader = () => {
           >
             Spark Collab
           </Title>
+
+          <Dropdown
+            menu={{
+              items: boards.map((board) => ({
+                key: board.id,
+                label: board.title,
+                onClick: () => navigate(`/board/${board.id}`),
+              })),
+            }}
+            trigger={["click"]}
+          >
+            <Button loading={loading}>
+              Select Board
+            </Button>
+          </Dropdown>
         </Flex>
 
         <Flex align="center" gap={12}>
@@ -47,7 +67,7 @@ const AppHeader = () => {
             Export
           </Button>
           <Dropdown
-            menu={{ items: menuItem }}
+            menu={{ items: menuItems }}
             trigger={["click"]}
           >
             <Avatar
