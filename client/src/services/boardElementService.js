95d08750770bb1f54a134bf539f41b5dc6fb6d3c
SaishSawant21 <saishsawant25102001@gmail.com>
2026-08-13 20:12:09 +0530

    Implemented board element service.

---
+++ client/src/services/boardElementService.js
@@ -0,0 +1,22 @@
+import { fetchApi, submitApi, updateApi } from './api';
+const prefix = '/boards';
+export const fetchBoardElements = (boardId) => {
+  return fetchApi(`${prefix}/${boardId}/elements`);
+}
+
+export const addBoardElement = (boardId, payload) => {
+  return submitApi(`${prefix}/${boardId}/elements`, payload);
+}
+
+export const updateBoardElement = (elementId, payload) => {
+  return updateApi(`${prefix}/${elementId}/elements`, payload);
+}
+
+export const saveBoardElement = async (element) => {
+  const payload = {
+    element_data: element?.element_data,
+    element_type: element?.element_type,
+  };
+
+  return await updateBoardElement(element?.id, payload);
+};
