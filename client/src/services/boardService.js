d2c0bcd58beabb52e30bdbcaa473e5d6cad4fc52
SaishSawant21 <saishsawant25102001@gmail.com>
2026-08-15 18:23:22 +0530

    Implemented boardService for client actions.

--- client/src/services/boardService.js
+++ client/src/services/boardService.js
@@ -1,6 +1,19 @@
-import { fetchApi } from "./api";
+import { deleteApi, fetchApi, submitApi, updateApi } from "./api";
 const prefix = "/boards";
 
 export const fetchBoards = () => {
 	return fetchApi(`${prefix}`);
+};
+
+export const createBoard = async (payload) => {
+	const response = await submitApi(`${prefix}`, payload);
+	return response;
+};
+
+export const deleteBoard = async (boardId) => {
+	return await deleteApi(`${prefix}/${boardId}`);
+};
+
+export const updateBoard = async (boardId, payload) => {
+	return await updateApi(`${prefix}/${boardId}`, payload);
 };
