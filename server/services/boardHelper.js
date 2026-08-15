f28c8f9819e2abb2be05c9308c0441b49c1d5137
SaishSawant21 <saishsawant25102001@gmail.com>
2026-08-15 18:22:51 +0530

    Added missing import.

--- server/services/boardHelper.js
+++ server/services/boardHelper.js
@@ -1,10 +1,11 @@
 import { getBoardByIdModel } from "../models/boardModel.js";
+import { createError } from "../utils/createError.js";
 
 export const checkIfBoardExist = async (boardId, ownerId = null) => {
   const checkBoard = await getBoardByIdModel(boardId, ownerId);
 
   if (!checkBoard) {
-    throw createError("Board not found", 404);
+    throw createError("Board not found", 400);
   }
   return checkBoard;
 };
