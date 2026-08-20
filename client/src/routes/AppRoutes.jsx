a6e6ea408a5657c133a7ee35cbad6f3459186cb1
SaishSawant21 <saishsawant25102001@gmail.com>
2026-08-20 19:49:13 +0530

    Updated routes.

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,45 +1,64 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Login from "../pages/Auth/Login";
-import Board from "../pages/Board/Board";
-import BoardProvider from "../context/BoardProvider";
-import AppLayout from "../layouts/AppLayout";
-import ProtectedRoute from "./ProtectedRoutes";
-import BoardRedirect from "./BoardRedirect";
-import Dashboard from "../pages/Dashboard";
 import Registration from "../pages/Auth/Registration";
 import ProfileUpdate from "../pages/Auth/ProfileUpdate";
+import Dashboard from "../pages/Dashboard";
+
+import Board from "../pages/Board/Board";
+import BoardProvider from "../context/BoardProvider";
+
+import BoardLayout from "../layouts/BoardLayout";
+
+import ProtectedRoute from "./ProtectedRoutes";
+import BoardRedirect from "./BoardRedirect";
+import AppLayout from "../layouts/AppLayout";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public */}
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
+
+        {/* Protected */}
         <Route element={<ProtectedRoute />}>
           <Route index element={<BoardRedirect />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/profile-update" element={<ProfileUpdate />} />
-          <Route
-            path="/board"
-            element={
-              <ProtectedRoute />
-            }
-          >
-            <Route index element={<BoardRedirect />} />
+
+          {/* Normal App Pages */}
+          <Route element={<AppLayout />}>
             <Route
-              path=":boardId"
-              element={
-                <BoardProvider>
-                  <AppLayout>
-                    <Board />
-                  </AppLayout>
-                </BoardProvider>
-              }
+              path="/dashboard"
+              element={<Dashboard />}
+            />
+
+            <Route
+              path="/my-profile"
+              element={<ProfileUpdate />}
             />
           </Route>
+
+          {/* Board */}
+          <Route
+            path="/board/:boardId"
+            element={
+              <BoardProvider>
+                <BoardLayout>
+                  <Board />
+                </BoardLayout>
+              </BoardProvider>
+            }
+          />
+
+          <Route
+            path="/board"
+            element={<BoardRedirect />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 };
+
 export default AppRoutes;
