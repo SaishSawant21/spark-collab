4c600b996b92f4eb8d77a6ebbf5c966583f1ad2f
SaishSawant21 <saishsawant25102001@gmail.com>
2026-09-03 11:50:24 +0530

    Changed db configuration from client to pool.

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: ".env.local" });
 const processENV = process.env;
-const db = new pg.Client({
+const db = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
