6c8264f3b250384c9475668e57e421afe2ee029a
SaishSawant21 <saishsawant25102001@gmail.com>
2026-09-11 10:15:53 +0530

    Added missing import.

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 const Login = lazy(() => import("../pages/Auth/Login"));
 
